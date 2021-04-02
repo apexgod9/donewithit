e174fc3de3a1aea82a46691ad4bbe61233fd7990
apex-blaze <ritish619@gmail.com>
2021-04-02 13:44:59 +0530

    added login and logout functionality

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,13 +11,19 @@ import CustomListItem from "../components/CustomListItem";
 import { auth, db } from "../firebase";
 
 const HomeScreen = ({ navigation }) => {
+  const signOutUser = () => {
+    auth.signOut().then(() => {
+      navigation.replace("Login");
+    });
+  };
+
   useLayoutEffect(() => {
     navigation.setOptions({
-      title: "Doramon",
+      title: "Birbal",
       headerStyle: { backgroundColor: "#b67162" },
       headerLeft: () => (
         <View style={{ marginLeft: 10 }}>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={signOutUser} activeOpacity={0.5}>
             <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
           </TouchableOpacity>
         </View>
